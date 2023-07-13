54631fe50c3fcabbad1e8ee6f1d48d61fb2d1784
jujuclip <wprhtkso@gmail.com>
2023-07-13 15:42:58 +0900

    springProject

---
+++ src/main/java/ssg/com/a/dao/MemberDao.java
@@ -0,0 +1,13 @@
+package ssg.com.a.dao;
+
+import ssg.com.a.dto.MemberDto;
+
+public interface MemberDao {
+	
+	int idcheck(String id);
+	
+	int addmember(MemberDto dto);
+	
+	MemberDto loginAf(MemberDto dto);
+	
+}
